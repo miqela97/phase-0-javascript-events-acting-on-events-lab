77bb85d20540829743998fe99d6e01fee80079ba
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-07-25 13:11:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+
+const dodger = document.getElementById('dodger');
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (event.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if (left + 40 < 400) {
+    dodger.style.left = `${left + 10}px`;
+  }
+}
